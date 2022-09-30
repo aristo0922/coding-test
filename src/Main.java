998568ff44420753915c1edae153cfd400a7f560
aristo <dkfud2121@naver.com>
2022-09-30 13:01:43 +0900

    백준 1271 엄청난 부자2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BigInteger money, members;
+        Scanner scan = new Scanner(System.in);
+        money = scan.nextBigInteger();
+        members = scan.nextBigInteger();
+
+        BigInteger result1 = money.divide(members);
+        BigInteger result2 = money.remainder(members);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }

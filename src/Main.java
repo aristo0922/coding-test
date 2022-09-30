b397a9cafa1aa0e10ea346d6238b7713a0499479
aristo <dkfud2121@naver.com>
2022-09-30 14:11:10 +0900

    백준 2738 : 행렬 덧셈

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        BigInteger money, members;
+        int line, column;
         Scanner scan = new Scanner(System.in);
-        money = scan.nextBigInteger();
-        members = scan.nextBigInteger();
 
-        BigInteger result1 = money.divide(members);
-        BigInteger result2 = money.remainder(members);
-        System.out.println(result1);
-        System.out.println(result2);
+        line = scan.nextInt();
+        column = scan.nextInt();
+
+        int [][] array1 =  new int[line][column];
+
+        for(int i=0; i<line; i++){
+            for(int j=0; j<column; j++){
+                array1[i][j]=scan.nextInt();
+            }
+        }
+
+        for(int i=0; i<line; i++){
+            for(int j=0; j<column; j++){
+                array1[i][j]+=scan.nextInt();
+            }
+        }
+
+        for(int i=0; i<line; i++){
+            for(int j=0;j<column;j++){
+                System.out.print(array1[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
     }
 }
